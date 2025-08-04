c8e59f12b397c6f6e88d5fffddd65f1a2e7fac46
Gae6one <onegae@libero.it>
2025-08-04 14:10:44 +0200

    Delete app directory

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>✅ L'app FJGO ora funziona!</h1>
-      <p>Stai visualizzando correttamente la pagina di test.</p>
-    </main>
-  );
-}

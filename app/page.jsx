525a3c5c8b44c482e2abd52ac939f7493a60ebeb
Gae6one <onegae@libero.it>
2025-08-04 03:52:03 +0200

    Crea pagina test funzionante

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>✅ L'app FJGO ora funziona!</h1>
+      <p>Stai visualizzando correttamente la pagina di test.</p>
+    </main>
+  );
+}

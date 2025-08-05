aaef5f62850e3820eb85ae24dcf26ecc260398b2
Gae <onegae@libero.it>
2025-08-05 11:37:09 +0200

    Add input component code

--- app/components/ui/components/ui/input.jsx
+++ app/components/ui/components/ui/input.jsx
@@ -1,1 +1,11 @@
+export function Input({ placeholder, type = "text", defaultValue }) {
+  return (
+    <input
+      type={type}
+      defaultValue={defaultValue}
+      placeholder={placeholder}
+      className="border border-gray-300 p-2 rounded w-full mb-2"
+    />
+  );
+}
 
